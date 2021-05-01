6ec2841688de721fc8dc188d5076ad85d6aa078d
tkeber <t.keber@gmail.com>
2021-05-01 17:43:58 +0200

    multistep build

---
+++ download_binary.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -x
+version=$1
+architecture=`uname -m`
+
+case $architecture in
+aarch64)
+  archive="linux/litecoin-${version}-aarch64-linux-gnu.tar.gz"
+  ;;
+armv7l)
+  archive="linux/litecoin-${version}-arm-linux-gnueabihf.tar.gz"
+  ;;
+x86_64)
+  archive="linux/litecoin-${version}-x86_64-linux-gnu.tar.gz"
+esac
+
+dir="https://download.litecoin.org/litecoin-${version}/"
+
+gpg --import thrasher-release.asc 
+
+
+curl -sko "SHA256SUMS.asc" "$dir/SHA256SUMS.asc"
+curl -sko "$archive" "$dir/$archive"
+
+gpg --verify SHA256SUMS.asc
+
+sha256sum --ignore-missing --check SHA256SUMS.asc
+
+tar xvfz "$archive" "litecoin-$version/bin/litecoind"
+mv "litecoin-$version/bin/litecoind" "/usr/local/bin/litecoind"
